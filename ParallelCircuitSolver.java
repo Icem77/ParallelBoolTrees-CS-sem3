2477237f688f6150a273f0ee6a4b01f5d56e9a13
Szymon Kruk <sk459204@students.mimuw.edu.pl>
2024-11-14 19:07:08 +0100

    hard reset

--- ParallelCircuitSolver.java
+++ ParallelCircuitSolver.java
@@ -10,24 +10,20 @@ import java.util.concurrent.LinkedBlockingQueue;
 import cp2024.circuit.Circuit;
 
 public class ParallelCircuitSolver implements CircuitSolver {
+
     // TODO pozwalaj na współbiezną obsługę wielu próśb
     // TODO mozliwie współbieznie obliczaj obwody
-
-    public ParallelCircuitSolver() {
-        this.workers = ExecutorService.newFixedThreadPoolO(8)
-    }
 
     @Override
     public CircuitValue solve(Circuit c) {
         // TODO niezwłocznie zwróć spejcalny obiekt reprezentujący wartość obwodu
 
-       return new ParallelCircuitValue(new LinkedBlockingQueue<Boolean>())
+        /* FIX ME */
+        throw new RuntimeException("Not implemented.");
     }
 
     @Override
     public void stop() {
-        // TODO zaprzestań przyjmowania zleceń
-        // TODO niezwłocznie zakończ wszystkie trwające obliczenia tego solvera
 
         /* FIX ME */
         throw new RuntimeException("Not implemented.");
--- ParallelCircuitValue.java
+++ ParallelCircuitValue.java
@@ -5,27 +5,12 @@ import cp2024.circuit.CircuitValue;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class ParallelCircuitValue implements CircuitValue {
-    private Future<Boolean> channel;
-
-    public ParallelCircuitValue(Future<Boolean> channel) {
-        this.channel = channel;
-    }
 
     @Override
     public boolean getValue() throws InterruptedException {
-        try {
-            Boolean result = this.channel.get();
-            return result;
-        } catch (ExecutionException e) {
-            if (e.getCause() instanceof CancellationException) {
-                System.out.println("Task got canceled at some point!");
-                throw new InterruptedException();
-            } else {
-                throw new RuntimeException("UNPREDICTED FAILURE OF TASK!");
-            }
-        }
+        /* FIX ME */
+        throw new RuntimeException("Not implemented.");
     }
 }
