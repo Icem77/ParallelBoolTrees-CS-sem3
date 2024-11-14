d198e1b4b45bc2783e6927ed431a5a863fea1019
Szymon Kruk <sk459204@students.mimuw.edu.pl>
2024-11-14 20:40:09 +0100

    CircuitValue getValue() and constructor, Solver stop() and constructor

--- ParallelCircuitSolver.java
+++ ParallelCircuitSolver.java
@@ -3,29 +3,52 @@ package cp2024.solution;
 import cp2024.circuit.CircuitSolver;
 import cp2024.circuit.CircuitValue;
 
+import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Future;
 
 import cp2024.circuit.Circuit;
 
 public class ParallelCircuitSolver implements CircuitSolver {
+    ExecutorService workers;
+    LinkedList<Future<?>> ogoingCircuitCalculations;
+    LinkedList<LinkedBlockingQueue<Boolean>> correspondingChannels;
+    private Boolean isStoped;
 
     // TODO pozwalaj na współbiezną obsługę wielu próśb
     // TODO mozliwie współbieznie obliczaj obwody
 
+    public ParallelCircuitSolver() {
+        this.workers = Executors.newFixedThreadPool(8);
+        this.ogoingCircuitCalculations = new LinkedList<>();
+        this.correspondingChannels = new LinkedList<>();
+        this.isStoped = false;
+    }
+
     @Override
     public CircuitValue solve(Circuit c) {
-        // TODO niezwłocznie zwróć spejcalny obiekt reprezentujący wartość obwodu
+        if (this.isStoped) {
+            return new BrokenCircuitValue();
+        }
 
-        /* FIX ME */
-        throw new RuntimeException("Not implemented.");
+        // Future<?> newCircuitTask = initNewCircuit(AfterTask);
+
+        return new BrokenCircuitValue();
     }
 
     @Override
     public void stop() {
+        if (!this.isStoped) {
+            this.isStoped = true;
 
-        /* FIX ME */
-        throw new RuntimeException("Not implemented.");
+            for (Future<?> calculation : ogoingCircuitCalculations) {
+                calculation.cancel(true);
+                correspondingChannels.removeFirst();
+            }
+
+            workers.shutdown();
+        }
     }
 }
